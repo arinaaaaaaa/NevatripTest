0c045d95b2b1f34c0c9494071d3d245bfceb6b66
arina krumm <92879566+arinaaaaaaa@users.noreply.github.com>
2022-10-18 22:37:15 +0300

    desktop version of block is ready

--- nevatrip-block/components/PlaceCard.js
+++ nevatrip-block/components/PlaceCard.js
@@ -1,27 +1,50 @@
+import styles from "../styles/components/PlaceCard.module.scss"
+
+function setFlagColor() {
+    let color =  Math.floor(Math.random()*2);
+    switch(color) {
+        case 0: return {backgroundColor: "#099CE8", color: "#fff"}
+        case 1: return {backgroundColor: "#FFD83B", color: "#323232"}
+    }
+}
+
 export default function PlaceCard(props) {
     return (
-        <div className="cardContainer">
-            <div className="imageSection">
-                <div className="flag">НОВИНКА</div>
-                <img src="" alt="" />
+        <div className={styles.cardContainer}>
+            <div className={styles.imageSection} style={{backgroundImage: `url(${props.image})`}}>
+                {props.flag ? <div style={setFlagColor()}>{props.flag}</div> : null}
             </div>
-            <div className="infoSection">
-                <p className="title"></p>
-                <div className="time">
-                    <img src={props.image} alt="" />
-                    <p></p>
+            <div className={styles.infoSection}>
+                <p className={styles.title}>{props.title}</p>
+                <div className={styles.time}>
+                    <img src="icons/time.svg" alt="" />
+                    <p>{props.time}</p>
                 </div>
-                <div className="programList">
-
-                </div>
-                <div className="priceSection">
+                <ul className={styles.programList}>
+                    {
+                        props.list ? props.list.map((item) => 
+                            props.list[props.list.length - 1] !== item ? <li className={styles.item}>{item}</li> :
+                            <li className={styles.item}>
+                                <div>
+                                    {item}
+                                    {props.times ? props.times.map((time) => 
+                                        <p>{time}</p>
+                                    ) : null}
+                                </div>
+                            </li>
+                        ) : null
+                    }
+                </ul>
+                <div className={styles.priceSection}>
                     <span>
-                        <p className="onlinePrice">
-                            
-                        </p>
-                        <p className="offlinePrice">
-
-                        </p>
+                        <div className={styles.onlinePrice}>
+                            {props.online} ₽
+                        </div>
+                        {props.offline ?
+                            <div className={styles.offlinePrice}>
+                                {props.offline} ₽ на причале
+                            </div>
+                        : null}
                     </span>
                     <button>Подробнее</button>
                 </div>
--- nevatrip-block/pages/index.js
+++ nevatrip-block/pages/index.js
@@ -3,13 +3,67 @@ import PlaceCard from '../components/PlaceCard';
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="cardsList">
-        <PlaceCard image="/images/place1.jpg"/>
-        <PlaceCard/>
-        <PlaceCard/>
-        <PlaceCard/>
-        <PlaceCard/>
+    <div className={styles.container}>
+      <div className={styles.cardsList}>
+      <PlaceCard
+          image="/images/place2.jpg"
+          flag="НОВИНКА"
+          time="2 часа"
+          title="Обзорная экскурсия по рекам и каналам с остановками Hop on Hop Off 2020"
+          list={[
+            "Билет на целый день",
+            "Неограниченное количество катаний",
+            "6 остановок у главных достопримечательностей",
+            "Ближайшие рейсы сегодня"
+          ]}
+          times={["12:00", "12:00", "12:00", "12:00"]}
+          online="900"
+          offline="1200"
+        />
+        <PlaceCard
+          image="/images/place3.jpg"
+          flag="КРУГЛЫЙ ГОД"
+          time="2 часа"
+          title="Обзорная экскурсия по рекам и каналам с остановками Hop on Hop Off 2020"
+          list={[
+            "Билет на целый день",
+            "Неограниченное количество катаний",
+            "6 остановок у главных достопримечательностей",
+            "Ближайшие рейсы сегодня"
+          ]}
+          times={["12:00", "12:00", "12:00", "12:00"]}
+          online="900"
+          offline="1200"
+        />
+        <PlaceCard
+          image="/images/place4.jpg"
+          time="2 часа"
+          title="Обзорная экскурсия по рекам и каналам с остановками Hop on Hop Off 2020"
+          list={[
+            "Билет на целый день",
+            "Неограниченное количество катаний",
+            "6 остановок у главных достопримечательностей",
+            "Ближайшие рейсы сегодня"
+          ]}
+          times={["12:00", "12:00", "12:00", "12:00"]}
+          online="900"
+          offline="1200"
+        />
+        <PlaceCard
+          image="/images/place1.jpg"
+          flag="НОВИНКА"
+          time="2 часа"
+          title="АКЦИЯ - Обзорная экскурсия по рекам и каналам с остановками Hop on Hop Off 2019"
+          list={[
+            "Билет на целый день",
+            "Неограниченное количество катаний",
+            "6 остановок у главных достопримечательностей",
+            "Ближайшие рейсы сегодня"
+          ]}
+          times={["12:00", "12:00", "12:00", "12:00"]}
+          online="900"
+          offline="1200"
+        />
       </div>
     </div>
   )
